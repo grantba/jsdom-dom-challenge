bc6858d102b33d8319080734f26d2c48178955e9
Blaire <grant.blaire@gmail.com>
2021-05-31 04:47:34 -0500

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -2,12 +2,14 @@ let counter = document.getElementById("counter");
 let seconds = 0;
 
 function incrementSeconds() {
+    // keeps the timer running unless isRunning returns false
     if (isRunning()) {
         seconds += 1;
         counter.innerText = seconds;
     }
 }
 
+// starts timer as soon as DOM is loaded
 let start = setInterval(incrementSeconds, 1000);
 
 let pause = document.getElementById("pause");
@@ -15,11 +17,13 @@ let minus = document.getElementById("minus");
 let plus = document.getElementById("plus");
 
 function isRunning() {
+    // returns true or false depending on if the innerText is equal to pause or resume
     return (pause.innerText === "pause" ? true : false);
 }
 
 pause.addEventListener('click', () => {
     pause.innerText = (pause.innerText === "pause" ? "resume" : "pause");
+    // iterates over all the buttons and toggles between disabled and enabled
     const buttons = [minus, plus, heart]
     buttons.forEach(button =>{
         button.disabled = !button.disabled;
@@ -38,18 +42,23 @@ minus.addEventListener('click', () => {
 
 let heart = document.getElementById("heart");
 let heartCommentArea = document.querySelector("body > ul");
+// sets up an object to gather timer number and how many times it's been clicked
 let heartLikes = {}
 
 heart.addEventListener("click", () => {
-    let valueOfSeconds = parseInt(seconds);
+    // captures value of seconds when clicked
+    let valueOfSeconds = seconds;
+    // sets how many times it's been clicked for a timer number to one if on the first click
+    // or increments the number by one for each subsequent click
     (heartLikes[valueOfSeconds] ? heartLikes[valueOfSeconds] += 1 : heartLikes[valueOfSeconds] = 1);
-    let li = document.createElement("li");
     if (document.getElementById(`like-${valueOfSeconds}`)) {
-        let likedLi = document.getElementById(`like-${valueOfSeconds}`)
-        likedLi.innerText = `${valueOfSeconds} has been liked ${heartLikes[valueOfSeconds]} times.`;
+        // if li already exists then innerText is just updated with new how many times it's been clicked value
+        document.getElementById(`like-${valueOfSeconds}`).innerText = `${valueOfSeconds} has been liked ${heartLikes[valueOfSeconds]} times.`;
     }
     else {
+        let li = document.createElement("li");
         heartCommentArea.appendChild(li);
+        // creates a new li and adds an id to verify later if this li already exists
         li.id = `like-${valueOfSeconds}`
         li.innerText = `${valueOfSeconds} has been liked 1 time.`;
     }
